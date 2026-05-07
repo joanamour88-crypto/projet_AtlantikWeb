5d5d5eacf44ccc1b7b5e8837ffbc70807cbef92a
joanamour88-crypto <joan.amour88@gmail.com>
2026-05-07 15:34:55 +0200

    UC7

---
+++ app/Views/Visiteur/vue_Reservetraversee.php
@@ -0,0 +1,7 @@
+<div class="card-body shadow d-flex justify-content-center align-items-center" style="text-align: center; height: 600px; width: 500px; margin: auto; margin-top: 100px;">
+    <?php
+        
+
+    ?>
+
+</div>
